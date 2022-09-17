436f2734407035fd1eb01ee5bf90d90b7bdf73cf
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-09-18 02:27:32 +0700

    Update Head - [Skip GitHub Action]

--- components/Head.jsx
+++ components/Head.jsx
@@ -5,28 +5,28 @@ export default function HeadMeta({ title }) {
   return (
     <>
       <Head>
-        <title>{title || 'Gatotkaca Search'}</title>
-        <meta name="title" content="Gatot Kaca Search Engine" />
+        <title>{title || 'Kominfo Search'}</title>
+        <meta name="title" content="Kominfo Search Engine" />
         <meta
           name="description"
-          content="Gatot Kaca Search Engine adalah search engine search enginan yang katanya bisa bersaing dengan google. Nyatanya kita masih bergantung dengan google, bahkan kita pun mencari tutorial membuat search engine di google"
+          content="Kominfo Search Engine adalah search engine search enginan yang katanya bisa bersaing dengan google. Nyatanya kita masih bergantung dengan google, bahkan kita pun mencari tutorial membuat search engine di google"
         />
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://carigatotkaca.my.id/" />
-        <meta property="og:title" content="Gatot Kaca Search Engine" />
+        <meta property="og:title" content="Kominfo Search Engine" />
         <meta
           property="og:description"
-          content="Gatot Kaca Search Engine adalah search engine search enginan yang katanya bisa bersaing dengan google. Nyatanya kita masih bergantung dengan google, bahkan kita pun mencari tutorial membuat search engine di google"
+          content="KOminfo Search Engine adalah search engine search enginan yang katanya bisa bersaing dengan google. Nyatanya kita masih bergantung dengan google, bahkan kita pun mencari tutorial membuat search engine di google"
         />
         <meta property="og:image" content="/yntkts.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://carigatotkaca.my.id/" />
-        <meta property="twitter:title" content="Gatot Kaca Search Engine" />
+        <meta property="twitter:url" content="https://search-kominfo.vercel.app/" />
+        <meta property="twitter:title" content="Kominfo Search Engine" />
         <meta
           property="twitter:description"
-          content="Gatot Kaca Search Engine adalah search engine search enginan yang katanya bisa bersaing dengan google. Nyatanya kita masih bergantung dengan google, bahkan kita pun mencari tutorial membuat search engine di google"
+          content="Kominfo Search Engine adalah search engine search engine yang katanya bisa bersaing dengan google. Nyatanya kita masih bergantung dengan google, bahkan kita pun mencari tutorial membuat search engine di google"
         />
         <meta property="twitter:image" content="/yntkts.png" />
       </Head>
