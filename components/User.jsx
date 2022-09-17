699f3268539f0c97d8eac3812c9d6e64d793124c
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-09-18 02:37:52 +0700

    Update User.jsx - [Skip GitHub Action]

--- components/User.jsx
+++ components/User.jsx
@@ -22,12 +22,12 @@ export default function User() {
         <div className="absolute right-8 top-[4.5rem] flex flex-col w-48 bg-white shadow-md rounded-md border border-slate-200 p-2">
           <div className="form-group flex flex-row gap-3 rounded-md hover:bg-yellow-400 p-2 items-center">
             <Image
-              src="/Avatar.png"
+              src="/kominfo.png"
               width={40}
               height="40"
               onClick={() => setIsOpen(!isOpen)}
             />
-            <h2 className="text-sm">Hi, Bro</h2>
+            <h2 className="text-sm">Kominfo Tolol</h2>
           </div>
           <div
             className="form-group flex flex-row gap-3 rounded-md hover:bg-yellow-400 p-2 items-center"
@@ -35,7 +35,7 @@ export default function User() {
           >
             <FiUser />
             <h2 className="text-sm">
-              <a href="https://bagusok.github.io/">Author</a>
+              <a href="https://github.com/bagussatoto/search.kominfo">Repo Github</a>
             </h2>
           </div>
         </div>
