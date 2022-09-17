30f3cf26c3675dbd861565f71426f6ea8a089712
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-09-18 02:24:06 +0700

    Update footer - [Skip GitHub Action]

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mt-12 w-full px-1 self-end place-self-end">
       <p className="text-xs font-normal ">
         Katanya
-        <span className="font-bold"> Gatot Kaca Search Engine</span>
+        <span className="font-bold"> Kominfo Search Engine</span>
       </p>
     </footer>
   );
