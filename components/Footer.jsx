c61ade7997d82f551a14791d86f648aa4aa8260e
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-02-24 07:34:46 +0700

    Amikom

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mt-12 w-full px-1 self-end place-self-end">
       <p className="text-xs font-normal ">
         Bagus Budi Satoto | 
-        <span className="font-bold"> [Universitas Amikom Yogyakarta](https://home.amikom.ac.id/) </span>
+        <span className="font-bold">Universitas Amikom Yogyakarta</span>
       </p>
     </footer>
   );
