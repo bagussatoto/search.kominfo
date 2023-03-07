080d691da20146de7ca48d90e1a3874d6d324291
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-03-07 21:38:44 +0700

    Update ``hitamkan universitas``

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="mt-12 w-full px-1 self-end place-self-end">
       <p className="text-xs font-normal ">
         Bagus Budi Satoto | 
-        <span className="font-bold">Universitas Amikom Yogyakarta</span>
+        <span className="font-bold"><b> Universitas Amikom Yogyakarta</b></span>
       </p>
     </footer>
   );
