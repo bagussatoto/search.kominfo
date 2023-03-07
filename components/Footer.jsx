451b30928879c2ab463351577f671cf2fc758a93
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-03-07 21:45:17 +0700

    Update Fitur ``Instagram``

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="mt-12 w-full px-1 self-end place-self-end">
       <p className="text-xs font-normal ">
-        Bagus Budi Satoto | 
-        <span className="font-bold"><b><a href="https://home.amikom.ac.id/">Universitas Amikom Yogyakarta</a></b></span>
+        <b><a href="https://www.instagram.com/bagus_satoto1/">Bagus Budi Satoto</a></b> 
+        <span className="font-bold"><a href="https://home.amikom.ac.id/">Universitas Amikom Yogyakarta</a></span>
       </p>
     </footer>
   );
