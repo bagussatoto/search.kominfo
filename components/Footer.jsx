796b65fdf90951ba083c7420eb96ee060c24b38e
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2023-03-07 21:47:31 +0700

    Update ``Name IG``

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-12 w-full px-1 self-end place-self-end">
       <p className="text-xs font-normal ">
-        <b><a href="https://www.instagram.com/bagus_satoto1/">Bagus Budi Satoto</a></b> | 
+        <b><a href="https://www.instagram.com/bagus_satoto1/">Bagus Budi Satoto | </a></b>
         <span className="font-bold"><a href="https://home.amikom.ac.id/"> Universitas Amikom Yogyakarta</a></span>
       </p>
     </footer>
